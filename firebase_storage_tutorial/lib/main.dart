ab4540253be32c46a496d173fbf3689bfabd3115
PeterHdd <peterhaddadn@gmail.com>
2024-08-27 01:42:58 +0200

    chore: update firebase storage source code

--- firebase_storage_tutorial/lib/main.dart
+++ firebase_storage_tutorial/lib/main.dart
@@ -3,13 +3,14 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firestore_flutter/firebase_options.dart';
 import 'package:firestore_flutter/second_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
